5307013986bbbc5f1c53788c639bd5864073acce
Mark <mark.n.bacon.jr@gmail.com>
2022-04-25 13:58:37 -0400

    commit tree 60b7919f05ea5418852a8ec31f9ad708a33f996c

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+require "pry"
+
+def line (katz_deli)
+    line = "The line is currently:"
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else katz_deli.map.with_index(1) do |person, index|
+        line << " #{index}. #{person}"
+    end
+    puts line
+end
+end
+
+def take_a_number (katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+end
+end
